0497d53a05d528afe2fa36696c3d9110f08f33b6
iurie <nabiiru7@gmail.com>
2025-11-22 14:52:04 +0700

    feat: update API base URLs in .env.example and add login API constants

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,20 @@ import AppRoutes from "@/routes";
 function App() {
 
   // Temporary test API call
-  //   useEffect(() => {
-  //   fetch(import.meta.env.VITE_BASE_URL_API_DOMAIN + "/api/v1/auth/login", {
-  //     method: "POST",
-  //     headers: { 
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       email: "admin-umkm@yopmail.com",
-  //       password: "test",
-  //     }),
-  //   })
-  //     .then((res) => res.json())
-  //     .then((data) => console.log("API OK:", data))
-  //     .catch((err) => console.error("Error:", err));
-  // }, []);
+  
+  // fetch(`${import.meta.env.VITE_BASE_URL_API_DOMAIN}/auth/login`, {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     email: "admin-umkm@yopmail.com",
+  //     password: "test",
+  //   }),
+  // })
+  //   .then(res => res.json())
+  //   .then(data => console.log("API OK:", data))
+  //   .catch(err => console.error("Error:", err));
 
   // Jangan buka komentarnya kalo ga mau dipake
 
